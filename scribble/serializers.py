6464addca73ae4f12abea14dd2870c1697965615
esin87 <esin.saribudak@gmail.com>
2020-06-08 16:03:19 -0500

    Define Post serializer

---
+++ scribble/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    post_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='post_detail')
+    author = serializers.ReadOnlyField(source='author.name')
+
+    class Meta:
+        model = Post
+        fields = ('title', 'date', 'body', 'author', 'id')
