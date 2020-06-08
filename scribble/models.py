2334643257f50dbc9d60875570e2e74dcf5332b8
esin87 <esin.saribudak@gmail.com>
2020-06-08 15:59:54 -0500

    Add Post model

--- scribble/models.py
+++ scribble/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    body = models.TextField()
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
--- settings.sql
+++ settings.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS scribble;
+DROP USER IF EXISTS scribbleuser;
+
 CREATE DATABASE scribble;
 CREATE USER scribbleuser
 WITH PASSWORD 'scribble';
