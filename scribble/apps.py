b172d65c07ea88d8972270c8913ff1e249be330e
esin87 <esin.saribudak@gmail.com>
2020-06-08 15:44:15 -0500

    Initial commit

---
+++ scribble/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ScribbleConfig(AppConfig):
+    name = 'scribble'
---
+++ settings.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE scribble;
+CREATE USER scribbleuser
+WITH PASSWORD 'scribble';
+GRANT ALL PRIVILEGES ON DATABASE scribble TO scribbleuser;
