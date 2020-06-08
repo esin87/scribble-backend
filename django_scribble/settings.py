4bca4ad8bbd3ad228ced9125ec7effe3eabf1ce4
esin87 <esin.saribudak@gmail.com>
2020-06-08 15:50:46 -0500

    Create User app and custom user model

--- django_scribble/settings.py
+++ django_scribble/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scribble',
+    'rest_framework',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -116,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Register your models here.
+
+from . import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['id', 'email', 'name', 'is_superuser', 'last_login']
+    # The fieldsets are used when you edit a new user via the admin site.
+    # fieldsets is a list in the form of two tuples, where each pair represents an
+    # html <fieldset> on the admin page.  The tuples are in the format:
+    # (name, field_options), where name is a string representing the title of
+    # the fieldset and field_options is a dictionary of information about the
+    # fieldset including the list of fields.
+    # Below we're saying create 4 sections, the first section has no name specified
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions',
+         {
+             'fields': (
+                 'is_active',
+                 'is_staff',
+                 'is_superuser',
+             )
+         }
+         ),
+        ('Dates', {'fields': ('last_login',)}),
+    )
+    # add_fieldsets is similar to fieldsets but it is used specifically
+    # when you create a new user:
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2')
+        }),
+    )
+
+
+# register the model and tell Django to use the above UserAdmin
+# class to format the pages:
+admin.site.register(models.User, UserAdmin)
