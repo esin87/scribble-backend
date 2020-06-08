2e8d9f02e2a40900c9ffe8289525e6585fd72b49
esin87 <esin.saribudak@gmail.com>
2020-06-08 16:06:37 -0500

    Add Post Views

--- scribble/views.py
+++ scribble/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from .serializers import PostSerializer
+from .models import Post
 # Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
